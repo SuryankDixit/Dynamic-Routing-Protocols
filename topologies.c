34e1fc58a4de3c34a74cd9609072caf47511b1b0
Suryank Dixit <49695800+SuryankDixit@users.noreply.github.com>
2020-09-04 22:49:45 +0530

    Add files via upload

---
+++ topologies.c
@@ -0,0 +1,39 @@
+#include "graph.h"
+
+void buildTopology(){
+
+    int vertex = 3;
+    int edges = 3;
+    graph *topology = createGraph(vertex,edges);  	// Graph creation
+
+    /*
+				   
+				   +----------+
+		              0/2 |   R0      | 0/0
+		 +----------------+           +--------------------------+
+		 |     	   | 	       |                          |
+		 |                +-----------+                          |
+		 |                                                       |
+		 |                                                       |
+		 |                                                       |
+		 |                                            	   |
+		 |0/2                                                   |0/1
+	     +---+---+                                              +----+-----+
+	     |       |0/1                                        0/2|          |
+	     | R2    +----------------------------------------------+    R1    |
+	     |       |                      	      		      |          |
+	     +-------+                                              +----------+
+	   						
+
+*/
+    node *router0 = & (topology->routersArray[0]);
+    node *router1 = & (topology->routersArray[1]);
+    node *router2 = & (topology->routersArray[2]);
+
+    addEdge(router0,router1,"eth0/0","eth0/1",10);
+    addEdge(router1,router2,"eth0/2","eth0/1",15);
+    addEdge(router2,router0,"eth0/2","eth0/2",20);
+
+    printGraph(topology);
+}
+
